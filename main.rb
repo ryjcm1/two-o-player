c4007c25dfadbd30f2feede385157abe0f5f67c9
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-07 14:39:12 -0500

    add main game logic

--- main.rb
+++ main.rb
@@ -1,6 +1,29 @@
-require "./player"
+require "./question"
+require "./game"
+require "./turn"
 
-player1 = Player.new
-puts player1.lives
-player1.decrease_lives
-puts player1.lives
+# player1 = Player.new
+# puts player1.lives
+# player1.decrease_lives
+# puts player1.lives
+
+#initialize game
+game = Game.new
+turn = Turn.new
+
+while game.active do
+    current_player = (turn.current_player == 1 ? game.player1 : game.player2)
+    question = Question.new
+    puts "Player #{turn.current_player}: " + question.ask_question
+    answer = gets.chomp.to_i
+    if question.check_answer(answer) 
+      puts "P1: YES! You are correct"
+    else
+      puts "Player #{turn.current_player}: Seriously? No!"
+      current_player.decrease_lives
+    end
+    game.check_winner
+    turn.change_player
+
+  
+end
