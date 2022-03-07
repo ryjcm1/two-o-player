dd2b3030871299f6667f6abbcb61099b324910eb
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-07 14:18:02 -0500

    add game class

---
+++ game.rb
@@ -0,0 +1,43 @@
+require "./player"
+
+class Game
+
+  attr_reader :player1, :player2, :active
+
+  def initialize
+    @active = true
+    @player1 = Player.new
+    @player2 = Player.new
+
+  end
+
+  def end_game
+    @active = false
+  end
+
+  def check_winner
+    if @player1.lives == 0
+      puts "Player 2 wins with a score of #{@player2.lives}/3"
+      end_game
+    elsif @player2.lives == 0
+      puts "Player 1 wins with a score of #{@player1.lives}/3"
+      end_game
+    else
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+    end
+  end
+
+end
+
+# game = Game.new
+# puts game.player1.lives
+# puts game.player2.lives
+# puts game.active
+
+# game.player1.decrease_lives
+# game.player1.decrease_lives
+# game.player1.decrease_lives
+
+
+# game.check_winner
+# game.end_game
